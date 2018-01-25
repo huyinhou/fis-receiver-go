e32f9a1ff53f08f3073e42d696592724053260c0
胡银厚 <huyinhou@huyinhous-MacBook-Pro.local>
2018-01-25 18:29:15 +0800

    commit code

---
+++ README.md
@@ -0,0 +1,13 @@
+## fis用法
+```
+$ npm install -g fis3
+$ fis3 release rd
+```
+## glide用法
+```
+$ go get github.com/Masterminds/glide
+$ glide init
+$ glide get github.com/gorilla/mux
+```
+## 参考文档
+[fis发布到远端机器](http://fis.baidu.com/fis3/docs/beginning/debug.html#%E5%8F%91%E5%B8%83%E5%88%B0%E8%BF%9C%E7%AB%AF%E6%9C%BA%E5%99%A8)
---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/golang/glog"
+	"github.com/gorilla/mux"
+	"github.com/spf13/pflag"
+)
+
+type ReceiverOptions struct {
+	Port int
+}
+
+func mkdirAll(path string) bool {
+	dirpath := filepath.Dir(path)
+	_, err := os.Stat(dirpath)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		if err = os.MkdirAll(dirpath, os.ModePerm); err != nil {
+			return false
+		}
+		return true
+	}
+	return false
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("I'm ready for that, you know."))
+		return
+	}
+	err := r.ParseMultipartForm(int64(10 << 20))
+	if err != nil {
+		glog.Error("parse form failed.")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	saveto := r.Form.Get("to")
+	if !filepath.IsAbs(saveto) || !mkdirAll(saveto) {
+		glog.Errorf("create dest dir %s failed.", saveto)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	infile, _, err := r.FormFile("file")
+	if err != nil {
+		glog.Error("read form file failed: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	outfile, err := os.OpenFile(saveto, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		glog.Errorf("create file %s failed. error: %v", saveto, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	_, err = io.Copy(outfile, infile)
+	if err != nil {
+		glog.Error("copy file failed: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("1"))
+}
+
+func (o *ReceiverOptions) addFlags(fs *pflag.FlagSet) {
+	fs.IntVarP(&o.Port, "port", "p", 8200, "receiver port")
+}
+
+func startServer(o *ReceiverOptions) {
+	router := mux.NewRouter()
+	router.HandleFunc("/", handler)
+	mux := http.NewServeMux()
+	mux.Handle("/", router)
+	http.ListenAndServe(fmt.Sprintf(":%d", o.Port), mux)
+}
+
+func initFlags() {
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+	flag.CommandLine.Parse([]string{})
+	pflag.VisitAll(func(flag *pflag.Flag) {
+		glog.V(4).Infof("FLAG: --%s=%q", flag.Name, flag.Value)
+	})
+}
+
+func main() {
+	opts := &ReceiverOptions{}
+	opts.addFlags(pflag.CommandLine)
+	initFlags()
+	startServer(opts)
+}
